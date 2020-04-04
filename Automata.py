fd5d7aca7516af72c853e34ac4a754f07e2545f5
Luca Nigro <51754869+lnigro4@users.noreply.github.com>
2020-04-04 09:50:09 +0200

    risoluzione problemi path

--- Automata.py
+++ Automata.py
@@ -1,7 +1,16 @@
 import os
 
-os.system('git clone https://github.com/pcm-dpc/COVID-19 /Users/nigresson/Desktop/COVID19/COVID-19')
-os.system('python3 /Users/nigresson/Desktop/COVID19/Analisi\ dati/Nazione.py')
-os.system('python3 /Users/nigresson/Desktop/COVID19/Analisi\ dati/Regione.py')
-os.system('python3 /Users/nigresson/Desktop/COVID19/Analisi\ dati/Provincia.py')
-os.system('python3 /Users/nigresson/Desktop/COVID19/Analisi\ dati/Regioni.py')
+os.system('rmdir COVID-19')
+os.system('git clone https://github.com/pcm-dpc/COVID-19')
+os.system('python3 Nazione.py')
+os.system('python3 Regione.py')
+os.system('python3 Provincia.py')
+os.system('python3 Regioni.py')
+
+os.system('git add growth\ k-factor\ comparison.png')
+os.system('git add k\ factor\ -\ Lombardia.png')
+os.system('git add k\ factor\ -\ Milano.png')
+os.system('git add k\ factor.png')
+os.system('git add README.md')
+os.system('git commit -m "aggiornamento dati del giorno"')
+os.system('git push -u origin master')
